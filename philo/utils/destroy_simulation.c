949a852c0607ee561a39594cd919558c2b1cced8
Jonas ALLERHAND <jallerha@student.42.fr>
2022-08-11 17:04:59 +0200

    destroy simulation when it ends

---
+++ philo/utils/destroy_simulation.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   destroy_simulation.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/11 16:58:14 by jallerha          #+#    #+#             */
+/*   Updated: 2022/08/11 17:00:22 by jallerha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philosophers.h"
+
+/**
+ * @brief Destroy all mutexes and threads cleanly
+ * This function asserts that everything is correctly initialized
+ * and that the simulation ended properly. (all threads are joined)
+ * 
+ * @param settings Pointer to settings structure
+ */
+void	ft_destroy_simulation(t_settings *settings)
+{
+	ft_destroy_forks(settings->n_philos, settings);
+}
