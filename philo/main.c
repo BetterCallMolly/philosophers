6af6216a8e3a8a18f76befc47f97edee4095a5b0
Jonas ALLERHAND <jallerha@student.42.fr>
2022-08-11 17:05:28 +0200

    special case & clean initialization

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 12:31:21 by jallerha          #+#    #+#             */
-/*   Updated: 2022/08/11 16:36:44 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/08/11 17:04:15 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,13 @@ int	main(int argc, char **argv)
 	if (!(argc == 5 || argc == 6))
 		return (ft_error(ERROR_ARGUMENTS, 2));
 	sanity_flag = ft_parse(argc, argv + 1, &settings);
-	ft_print_settings(&settings);
 	if (sanity_flag > 0)
 		return (ft_lint(sanity_flag, argv, argc, sanity_flag));
+	ft_print_settings(&settings);
+	if (settings.n_philos == 1)
+		return (ft_alone());
+	if (ft_create_forks(settings.n_philos, &settings) == -1)
+		return (ft_error("Error creating forks", 2));
+	ft_destroy_simulation(&settings);
 	return (0);
 }
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 12:36:09 by jallerha          #+#    #+#             */
-/*   Updated: 2022/08/11 16:36:15 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/08/11 17:02:46 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define PHILOSOPHERS_H
 # include <unistd.h>
 # include <pthread.h>
+# include <stdlib.h>
 
 # define STDOUT 1
 # define STDERR 2
@@ -28,7 +29,7 @@ typedef struct s_settings
 	int				time_to_sleep;
 	int				n_meals;
 	int				limited_meals;
-	pthread_mutex_t	*start;
+	pthread_mutex_t	*forks;
 }	t_settings;
 
 int		ft_putstr_fd(char *string, int fd);
@@ -42,6 +43,15 @@ void	ft_bzero(void *s, size_t n);
 int		ft_error(char *message, int return_code);
 int		ft_parse(int argc, char **argv, t_settings *settings);
 int		ft_lint(int faulty_index, char **argv, int argc, int return_code);
+
+// Forks management
+int		ft_destroy_forks(int count_init, t_settings *settings);
+int		ft_create_forks(int count, t_settings *settings);
+void	ft_destroy_simulation(t_settings *settings);
+
+// Misc functions
+int		ft_alone(void);
+
 // Debug functions
 void	ft_print_settings(t_settings *settings);
 
