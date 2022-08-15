765345309668ed4aa1de1e79780333a8f3cd9a3f
Jonas ALLERHAND <jallerha@student.42.fr>
2022-08-15 16:48:01 +0200

    init misc mutexes, print help if necessary

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 12:31:21 by jallerha          #+#    #+#             */
-/*   Updated: 2022/08/11 17:04:15 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/08/15 15:44:57 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,17 @@ int	main(int argc, char **argv)
 
 	ft_bzero(&settings, sizeof(t_settings));
 	if (!(argc == 5 || argc == 6))
-		return (ft_error(ERROR_ARGUMENTS, 2));
+		return (ft_print_help(argv[0]));
 	sanity_flag = ft_parse(argc, argv + 1, &settings);
 	if (sanity_flag > 0)
 		return (ft_lint(sanity_flag, argv, argc, sanity_flag));
 	ft_print_settings(&settings);
-	if (settings.n_philos == 1)
-		return (ft_alone());
+	// if (settings.n_philos == 1)
+	// 	return (ft_alone());
 	if (ft_create_forks(settings.n_philos, &settings) == -1)
 		return (ft_error("Error creating forks", 2));
+	if (ft_misc_mutexes(&settings) == -1)
+		return (ft_error("Error creating mutexes", 2));
 	ft_destroy_simulation(&settings);
 	return (0);
 }
