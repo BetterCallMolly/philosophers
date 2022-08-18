5127cdbe51e9b10a66800bcf5c44b8693d989138
Jonas ALLERHAND <jallerha@student.42.fr>
2022-08-18 10:57:44 +0200

    enhance of error logging

--- philo/utils/ft_error.c
+++ philo/utils/ft_error.c
@@ -6,15 +6,19 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 12:50:31 by jallerha          #+#    #+#             */
-/*   Updated: 2022/08/11 16:35:37 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/08/17 15:06:10 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philosophers.h"
+#include "../colors.h"
 
 int	ft_error(char *message, int return_code)
 {
+	ft_putstr_fd(RED2, STDERR);
+	ft_putstr_fd("ERROR: ", STDERR);
 	ft_putstr_fd(message, STDERR);
+	ft_putstr_fd(RESET, STDERR);
 	ft_putendl_fd("", STDERR);
 	return (return_code);
 }
