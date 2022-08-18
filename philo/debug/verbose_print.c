072a28ef9eef8c1bc1b83b41462305fbfe3ad3d8
Jonas ALLERHAND <jallerha@student.42.fr>
2022-08-18 10:57:00 +0200

    verbose printing for debug mode

---
+++ philo/debug/verbose_print.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   verbose_print.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/17 15:53:39 by jallerha          #+#    #+#             */
+/*   Updated: 2022/08/18 10:56:52 by jallerha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philosophers.h"
+
+void	ft_verbose(char *message, char *file, int line)
+{
+	if (!DEBUG)
+		return ;
+	ft_putstr_fd("[:", STDERR);
+	ft_putstr_fd(file, STDERR);
+	ft_putstr_fd(":", STDERR);
+	ft_putnbr_fd(line, STDERR);
+	ft_putstr_fd("] ", STDERR);
+	ft_putstr_fd(message, STDERR);
+}
