c78ae417605b3f8b3258e3df25d8c60fa392fad5
Jonas ALLERHAND <jallerha@student.42.fr>
2022-08-18 10:57:14 +0200

    free philosopher array properly

--- philo/utils/destroy_simulation.c
+++ philo/utils/destroy_simulation.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 16:58:14 by jallerha          #+#    #+#             */
-/*   Updated: 2022/08/15 15:18:24 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/08/17 15:50:42 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,5 @@
 void	ft_destroy_simulation(t_settings *settings)
 {
 	ft_destroy_forks(settings->n_philos, settings);
+	free(settings->philos);
 }
