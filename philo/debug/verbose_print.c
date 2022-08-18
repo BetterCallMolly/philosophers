ff504b356b60bc326d066aac3fe9137b0cddf074
Jonas ALLERHAND <jallerha@student.42.fr>
2022-08-18 12:27:26 +0200

    changed multiple calls to a single printf

--- philo/debug/verbose_print.c
+++ philo/debug/verbose_print.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 15:53:39 by jallerha          #+#    #+#             */
-/*   Updated: 2022/08/18 11:01:34 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/08/18 11:06:27 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,5 @@ void	ft_verbose(char *message, char *file, int line)
 {
 	if (!DEBUG)
 		return ;
-	ft_putstr_fd("[", STDERR);
-	ft_putstr_fd(file, STDERR);
-	ft_putstr_fd(":", STDERR);
-	ft_putnbr_fd(line, STDERR);
-	ft_putstr_fd("] ", STDERR);
-	ft_putstr_fd(message, STDERR);
+	printf("[%s:%d] %s\n", file, line, message);
 }
