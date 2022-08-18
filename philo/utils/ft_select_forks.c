524e71fd5aa20f12b21da31f1059c13250666b91
Jonas ALLERHAND <jallerha@student.42.fr>
2022-08-19 01:01:28 +0200

    fixed fork selection algorithm to avoid deadlocks

--- philo/utils/ft_select_forks.c
+++ philo/utils/ft_select_forks.c
@@ -6,34 +6,40 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 16:39:52 by jallerha          #+#    #+#             */
-/*   Updated: 2022/08/17 15:30:44 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/08/19 00:26:14 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philosophers.h"
 
+/**
+ * @brief Select forks for philosopher, swaps them to avoid deadlock.
+ * 
+ * @param philos The philosopher that is eating.
+ * @param settings The settings of the simulation.
+ */
 void	ft_select_forks(t_philo *philos, t_settings *settings)
 {
-	int	i;
+	int				i;
+	int				fork_left_index;
+	int				fork_right_index;
+	int				swap_val;
 
 	i = 0;
 	while (i < settings->n_philos)
 	{
-		if (i == 0)
+		fork_left_index = i;
+		fork_right_index = i - 1;
+		if (fork_right_index < 0)
+			fork_right_index = settings->n_philos - 1;
+		if (fork_right_index > fork_left_index)
 		{
-			philos[i].left_fork = &settings->forks[settings->n_philos - 1];
-			philos[i].right_fork = &settings->forks[i];
+			swap_val = fork_left_index;
+			fork_left_index = fork_right_index;
+			fork_right_index = swap_val;
 		}
-		else if (i == settings->n_philos - 1)
-		{
-			philos[i].left_fork = &settings->forks[i - 1];
-			philos[i].right_fork = &settings->forks[0];
-		}
-		else
-		{
-			philos[i].left_fork = &settings->forks[i - 1];
-			philos[i].right_fork = &settings->forks[i];
-		}
+		philos[i].left_fork = &settings->forks[fork_left_index];
+		philos[i].right_fork = &settings->forks[fork_right_index];
 		i++;
 	}
 }
