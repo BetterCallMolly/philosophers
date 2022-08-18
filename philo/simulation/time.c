7c06d0f821392c76e80a0a69153bba1b07552d8a
Jonas ALLERHAND <jallerha@student.42.fr>
2022-08-19 01:02:08 +0200

    refactored timestamp function, start_time is stored in structs

--- philo/simulation/time.c
+++ philo/simulation/time.c
@@ -6,24 +6,18 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 11:12:53 by jallerha          #+#    #+#             */
-/*   Updated: 2022/08/18 11:17:29 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/08/19 00:38:17 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philosophers.h"
 
-long long int	ft_timestamp(void)
+long long	ft_timestamp(void)
 {
-	static long long int	start = 0;
-	struct timeval			tv;
-	long long int			timestamp;
+	struct timeval	tv;
+	long long		timestamp_ms;
 
-	if (start == 0)
-	{
-		gettimeofday(&tv, NULL);
-		start = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-	}
 	gettimeofday(&tv, NULL);
-	timestamp = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-	return (timestamp - start);
+	timestamp_ms = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	return (timestamp_ms);
 }
