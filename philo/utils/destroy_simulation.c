a5cf0c6b393a08e12651c12e4556e8a100d54fb0
Jonas ALLERHAND <jallerha@student.42.fr>
2022-08-19 01:01:51 +0200

    destroy thread array and misc mutexes

--- philo/utils/destroy_simulation.c
+++ philo/utils/destroy_simulation.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 16:58:14 by jallerha          #+#    #+#             */
-/*   Updated: 2022/08/17 15:50:42 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/08/19 00:26:46 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,9 @@
 void	ft_destroy_simulation(t_settings *settings)
 {
 	ft_destroy_forks(settings->n_philos, settings);
+	pthread_mutex_destroy(&settings->print_mutex);
+	pthread_mutex_destroy(&settings->state_mutex);
+	ft_destroy_meal_mutexes(settings);
+	free(settings->threads);
 	free(settings->philos);
 }
